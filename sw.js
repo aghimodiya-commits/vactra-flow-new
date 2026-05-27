f50c761ee46d5637c74220ed00c0dbe0de179664
vactra-flow <aghimodiya@gmail.com>
2026-05-27 23:43:04 +0530

    Add service worker caching for assets

--- sw.js
+++ sw.js
@@ -1,3 +1,10 @@
-self.addEventListener('fetch', (event) => {
-  event.respondWith(caches.match(event.request).then(r => r || fetch(event.request)));
+const CACHE_NAME = 'vactra-v1';
+const ASSETS = ['/', 'social-app-FIXED.html', '1024.png'];
+
+self.addEventListener('install', (e) => {
+  e.waitUntil(caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS)));
+});
+
+self.addEventListener('fetch', (e) => {
+  e.respondWith(caches.match(e.request).then((res) => res || fetch(e.request)));
 });
