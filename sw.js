c36c64b2062db0c25ac30da9ce04c30b72b6c128
vactra-flow <aghimodiya@gmail.com>
2026-05-27 22:33:53 +0530

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,6 +1,3 @@
-self.addEventListener('install', (e) => {
-  e.waitUntil(self.skipWaiting());
-});
-self.addEventListener('activate', (e) => {
-  e.waitUntil(self.clients.claim());
+self.addEventListener('fetch', (event) => {
+  event.respondWith(caches.match(event.request).then(r => r || fetch(event.request)));
 });
